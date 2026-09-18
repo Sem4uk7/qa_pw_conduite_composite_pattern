ecf1b5060fb712a95064539ba96b945a5b621e28
Anton <semtoshka@gmail.com>
2026-09-18 22:07:21 +0200

    Fix Authorization header when token is missing in CommentsApi

--- src/api/resources/CommentsApi.js
+++ src/api/resources/CommentsApi.js
@@ -9,23 +9,29 @@ export class CommentsApi extends BaseApi {
 
   async createComment(slug, comment, token = null) {
     return await this.step(`Create new comment`, async () => {
+      const headers = { ...this._headers };
+
+      if (token) {
+        headers.authorization = `Token ${token}`;
+      }
+
       return await this.client.post(ROUTES.comments(slug).index, {
         data: { comment },
-        headers: {
-          authorization: `Token ${token}`,
-          ...this._headers,
-        },
+        headers,
       });
     });
   }
 
   async deleteComment(slug, commentId, token = null) {
     return await this.step(`Delete comment`, async () => {
+      const headers = { ...this._headers };
+
+      if (token) {
+        headers.authorization = `Token ${token}`;
+      }
+
       return await this.client.delete(ROUTES.comments(slug, commentId).single, {
-        headers: {
-          authorization: `Token ${token}`,
-          ...this._headers,
-        },
+        headers,
       });
     });
   }
